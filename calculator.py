ca33d4bb9d01dc28c4ace57e48d67de4a9b2c345
lienne <lianasevero3@gmail.com>
2017-03-28 18:03:07 -0400

    First ideas

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
 #Simple Calculator
+
+def add():
+	x = raw_input()
+	y = raw_input()
+	result = x + y
+	return result
+
+def sub():
+	x = raw_input()
+	y = raw_input()
+	result = x - y
+	return result
+
+def div():
+	x = raw_input()
+	y = raw_input()
+	result = x/y
+	return result
+
+def mult():
+	x = raw_input()
+	y = raw_input()
+	result = x*y
